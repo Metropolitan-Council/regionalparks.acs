b26142c72cb76a4ce79a3a11bdf6b2c9e56b237d
Ellen Esch <Ellen.Esch@metc.state.mn.us>
2020-10-16 10:13:58 -0500

    update agency names
    per conversation w/ darcie 16 oct 2020

--- app.R
+++ app.R
@@ -8,3 +8,4 @@ regionalparks.acs::run_app() # add parameters here (if any)
 
 
 # rsconnect::deployApp(appDir = ".", appId = 2464148,  account = "metrotransitmn", server = "shinyapps.io", appName = "regional-parks-acs", appTitle = "regional-parks-acs", lint = FALSE, metadata = list(asMultiple = FALSE, asStatic = FALSE, ignoredFiles = "README.Rmd|.github/.gitignore|.github/workflows/R-CMD-check.yaml|data-raw/block_group.R|data-raw/census_tract.R|data-raw/park_trail_geog.R|data-raw/table_ct.R|dev/01_start.R|dev/02_dev.R|dev/03_deploy.R|dev/run_dev.R|man/pipe.Rd|man/run_app.Rd|tests/testthat.R|tests/testthat/test-golem-recommended.R"), logLevel = "verbose")
+ 
