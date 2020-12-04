87a9cc92ff73cc4264286b4cf51531b664d434f2
Liz Roten <liz.roten@metc.state.mn.us>
2020-12-04 15:19:08 -0600

    call input_demos_ui_1 and 2, leaflet_mod 1 and 2

--- R/app_server.R
+++ R/app_server.R
@@ -6,6 +6,14 @@
 #' @noRd
 app_server <- function(input, output, session) {
   # List the first level callModules here
+  
   tract_data <- callModule(mod_input_demos_server, "input_demos_ui_1")
   callModule(mod_leaflet_server, "leaflet_ui_1", tract_data)
+  
+  tract_data2 <- callModule(mod_input_demos_server, "input_demos_ui_2")
+  
+  callModule(mod_leaflet_server, "leaflet_ui_2", tract_data2)
+  
+  callModule(mod_summary_server, "summary_ui_1")
+  
 }
