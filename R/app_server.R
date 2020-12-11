0714ef6dac845e86df177b5d309d09e74f4ddc5f
Liz Roten <liz.roten@metc.state.mn.us>
2020-12-11 12:22:10 -0600

    remove calculations occurring in app_server

--- R/app_server.R
+++ R/app_server.R
@@ -21,45 +21,6 @@ app_server <- function(input, output, session) {
   )
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-  # callModule(mod_choice_server, "choice_ui_1")
-  filterData <- reactiveVal(long_buffer_data)
-  filtered_df <- reactive({
-    res <- filterData() %>% filter(
-      ACS == input$ACS,
-      agency %in% input$agency,
-      distance == input$distance,
-      type %in% input$type,
-      status %in% input$status
-    )
-    res
-  })
-
-  # callModule(mod_choice2_server, "choice2_ui_1")
-  filterData2 <- reactiveVal(long_buffer_data)
-  filtered_df2 <- reactive({
-    res2 <- filterData2() %>% filter(
-      ACS == input$ACS2,
-      agency %in% input$agency2,
-      distance == input$distance2,
-      type %in% input$type2,
-      status %in% input$status2
-    )
-    res2
-  })
-
   tract_data <- callModule(mod_input_demos_server, "input_demos_ui_1")
   callModule(mod_leaflet_server, "leaflet_ui_1", tract_data)
 
