0cb4d50ee3ca3645c2a669e43b60907f2533b702
Liz Roten <liz.roten@metc.state.mn.us>
2020-12-11 12:09:04 -0600

    use all input values when filtering the table view

--- R/mod_summary_utils.R
+++ R/mod_summary_utils.R
@@ -22,7 +22,11 @@ mod_summary_utils_server <- function(input, output, session,
   make_table_buffer_data <- reactive({
     # browser()
     p <- regionalparks.acs::long_buffer_data %>%
-      dplyr::filter(agency %in% selected_vars$input_agency)
+      dplyr::filter(agency %in% selected_vars$input_agency,
+                    type %in% selected_vars$input_type,
+                    distance == selected_vars$input_distance,
+                    status %in% selected_vars$input_status,
+                    ACS == selected_vars$input_acs)
     return(p)
   })
 
