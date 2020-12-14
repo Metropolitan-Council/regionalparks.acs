8619210d7572b874c174dcb980b953916a5951f7
Liz Roten <liz.roten@metc.state.mn.us>
2020-12-14 13:29:41 -0600

    remove "ACS" from tab titles
    makes navbar neater and narrower

--- R/app_ui.R
+++ R/app_ui.R
@@ -9,74 +9,83 @@ app_ui <- function(request) {
   tagList(
     # Leave this function for adding external resources
     golem_add_external_resources(),
-
+    
     # List the first level UI elements here
     navbarPage("Regional Parks and Trails Equity Tool",
-      id = "nav",
-
-
-      # intro tab -----
-      tabPanel(
-        "Introduction",
-        mod_intro_ui("intro_ui_1")
-      ),
-
-      ## map tab -----
-      tabPanel(
-        "ACS Map",
-        mod_leaflet_ui(id = "leaflet_ui_1"),
-
-        mod_input_demos_ui(id = "input_demos_ui_1"),
-      ),
-
-      # acs summary tab -----
-      tabPanel(
-        "ACS Summary",
-        mod_summary_selections_ui("summary_selections_ui_1"),
-        HTML("<p>Find data summarized in several ways in the tabs below. The <em>Weighted averages</em> tab distills complex spatial demographic patterns into a single summary statistic, and will be appropriate for most uses. The <em>Buffer map</em> tab shows the spatial patterns of population demographics with the buffer analysis zones overlayed. The <em>Download tabular data</em> tab displays the weighted average summary statistics for all ACS variables in tabular form.</p>"), # The <em>Methods and raw data</em> tab is intended for advanced users and illustrates the steps taken to aggregate demographic data into weighted averages
-        tabsetPanel(
-          selected = "Weighted averages",
-          tabPanel(
-            "Weighted averages",
-            mod_summary_plot_ui("summary_plot_ui_1")
-          ),
-          tabPanel(
-            "Buffer map",
-            mod_summary_map_ui("summary_map_ui_1")
-          ),
-          tabPanel(
-            "Download tabular data",
-            mod_summary_download_ui("summary_download_ui_1"),
-            mod_summary_table_ui("summary_table_ui_1")
-          ) # ,
-          # tabPanel("Methods and raw data",
-          #          mod_summary_raw_ui("summary_raw_ui_1"))
-        )
-      ),
-
-      # Pop growth tab -----
-      tabPanel(
-        "Population Growth",
-        tabsetPanel(
-          selected = "Population map",
-          tabPanel(
-            "Population map",
-            mod_pop_selections_ui("pop_selections_ui_1"),
-            mod_pop_map_ui("pop_map_ui_1")
-          ),
-          tabPanel(
-            "Demographic shifts",
-            mod_pop_demoshifts_ui("pop_demoshifts_ui_1")
-          )
-        )
-      ),
-
-
-      ## Notes tab -----
-      tabPanel(
-        "Notes",
-        mod_notes_ui("notes_ui_1")
-      )
+               id = "nav",
+               
+               
+               # intro tab -----
+               tabPanel(
+                 "Introduction",
+                 mod_intro_ui("intro_ui_1")
+               ),
+               
+               ## map tab -----
+               tabPanel(
+                 "Map",
+                 shiny::wellPanel(
+                   HTML("<p>Select 1 ACS variable. The tracts’ colors correspond to 2014-2018 (5-year) American Community Survey (ACS) demographic metric selected. Each demographic characteristic is shown as a percentage of the total population, with the exception of median household income, which is displayed in dollars. The darker the color, the higher the percentage (or income in dollars).</p>")),
+                 
+                 # sidebarPanel(
+                 mod_input_demos_ui(id = "input_demos_ui_1"),
+                 
+                 # ),
+                 mainPanel(
+                   mod_leaflet_ui(id = "leaflet_ui_1")) 
+                 
+               ),
+               
+               # acs summary tab -----
+               tabPanel(
+                 "Summary",
+                 sidebarPanel(mod_summary_selections_ui("summary_selections_ui_1")),
+                 mainPanel(
+                   HTML("<p>Find data summarized in several ways in the tabs below. The <em>Weighted averages</em> tab distills complex spatial demographic patterns into a single summary statistic, and will be appropriate for most uses. The <em>Buffer map</em> tab shows the spatial patterns of population demographics with the buffer analysis zones overlayed. The <em>Download tabular data</em> tab displays the weighted average summary statistics for all ACS variables in tabular form.</p>"), # The <em>Methods and raw data</em> tab is intended for advanced users and illustrates the steps taken to aggregate demographic data into weighted averages
+                   tabsetPanel(
+                     selected = "Weighted averages",
+                     tabPanel(
+                       "Weighted averages",
+                       mod_summary_plot_ui("summary_plot_ui_1")
+                     ),
+                     tabPanel(
+                       "Buffer map",
+                       mod_summary_map_ui("summary_map_ui_1")
+                     ),
+                     tabPanel(
+                       "Download tabular data",
+                       mod_summary_download_ui("summary_download_ui_1"),
+                       mod_summary_table_ui("summary_table_ui_1")
+                     ) # ,
+                     # tabPanel("Methods and raw data",
+                     #          mod_summary_raw_ui("summary_raw_ui_1"))
+                   )
+                 )
+               ),
+               
+               # Pop growth tab -----
+               tabPanel(
+                 "Population Growth",
+                 tabsetPanel(
+                   selected = "Population map",
+                   tabPanel(
+                     "Population map",
+                     mod_pop_selections_ui("pop_selections_ui_1"),
+                     mod_pop_map_ui("pop_map_ui_1")
+                   ),
+                   tabPanel(
+                     "Demographic shifts",
+                     mod_pop_demoshifts_ui("pop_demoshifts_ui_1")
+                   )
+                 )
+               ),
+               
+               
+               ## Notes tab -----
+               tabPanel(
+                 "Notes",
+                 mod_notes_ui("notes_ui_1")
+               )
     )
   )
 }
@@ -93,7 +102,7 @@ golem_add_external_resources <- function() {
   add_resource_path(
     "www", app_sys("app/www")
   )
-
+  
   tags$head(
     bundle_resources(
       path = app_sys("app/www"),
