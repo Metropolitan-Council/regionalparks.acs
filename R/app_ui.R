6ef0ef7af95fba1dfd438044274e9a53d89b2b86
Ellen Esch <Ellen.Esch@metc.state.mn.us>
2021-01-06 14:47:20 -0600

    add logo

--- R/app_ui.R
+++ R/app_ui.R
@@ -11,7 +11,7 @@ app_ui <- function(request) {
     golem_add_external_resources(),
 
     # List the first level UI elements here
-    navbarPage("Regional Parks and Trails Equity Tool",
+    navbarPage(title = div(img(src="www/main-logo.png")),
       id = "nav",
 
 
