8c98029532f6713adcd773909cc574ad16d03672
Ellen Esch <Ellen.Esch@metc.state.mn.us>
2021-01-07 16:44:37 -0600

    add data

--- data-raw/transit_routes.R
+++ data-raw/transit_routes.R
@@ -12,18 +12,18 @@ library(tigris)
 library(janitor)
 
 
-## transit routes ---------------
-temp <- tempfile()
-download.file("ftp://ftp.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_metc/trans_transit_routes/gpkg_trans_transit_routes.zip",
-  destfile = temp
-)
-trans_routes <- sf::read_sf(unzip(temp, "trans_transit_routes.gpkg")) %>%
-  st_transform(4326)
-
-fs::file_delete("trans_transit_routes.gpkg")
-
-usethis::use_data(trans_routes, overwrite = TRUE)
-
+# ## transit routes ---------------
+# temp <- tempfile()
+# download.file("ftp://ftp.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_metc/trans_transit_routes/gpkg_trans_transit_routes.zip",
+#   destfile = temp
+# )
+# trans_routes <- sf::read_sf(unzip(temp, "trans_transit_routes.gpkg")) %>%
+#   st_transform(4326)
+# 
+# fs::file_delete("trans_transit_routes.gpkg")
+# 
+# usethis::use_data(trans_routes, overwrite = TRUE)
+# 
 
 
 ## transit stops ---------------
