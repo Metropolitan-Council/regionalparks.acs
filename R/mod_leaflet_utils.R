1ae84a989f131cbf8d6e6723f90c174275ec37fd
Liz Roten <liz.roten@metc.state.mn.us>
2021-01-08 14:28:33 -0600

    remove data processing
    this processing  has already happened with the "duplicate" datasets

--- R/mod_leaflet_utils.R
+++ R/mod_leaflet_utils.R
@@ -26,55 +26,12 @@ mod_leaflet_utils_server <- function(input, output, session,
   make_leafletacs_data <- reactive({
     p6 <-  
       if (selected_map_vars$input_acsmap %in% tract_vars$ACS) {
-        regionalparks.acs::census_tract %>%
-          mutate(
-            disab_percent = `Disability, any disability` * 100,
-            usborn_percent = `Origin, US-born` * 100,
-            forborn_percent = `Origin, foreign-born` * 100
-          ) %>%
-          rename(
-            "adj_anydis_per" = "disab_percent",
-            "adj_usborn_per" = "usborn_percent",
-            "adj_forborn_per" = "forborn_percent"
-          ) %>%
+        regionalparks.acs::census_tract_map %>%
           select(selected_map_vars$input_acsmap)
       }
     
     else {
-      regionalparks.acs::block_group %>%
-        mutate(
-          ageunder15_percent = ageunder15_percent * 100,
-          age15_24_percent = age15_24_percent * 100,
-          age25_64_percent = age25_64_percent * 100,
-          age65up_percent = age65up_percent * 100,
-          whitenh_percent = whitenh_percent * 100,
-          blacknh_percent = blacknh_percent * 100,
-          asiannh_percent = asiannh_percent * 100,
-          amindnh_percent = amindnh_percent * 100,
-          othermultinh_percent = othermultinh_percent * 100,
-          hisppop_percent = hisppop_percent * 100,
-          nothisppop_percent = nothisppop_percent * 100,
-          novehicle_percent = novehicle_percent * 100,
-          poorenglish_percent = poorenglish_percent * 100,
-          spanish_percent = spanish_percent * 100
-        ) %>%
-        rename(
-          "adj_ageunder15_per" = "ageunder15_percent",
-          "adj_age15_24_per" = "age15_24_percent",
-          "adj_age25_64_per" = "age25_64_percent",
-          "adj_age65up_per" = "age65up_percent",
-          "adj_whitenh_per" = "whitenh_percent",
-          "adj_blacknh_per" = "blacknh_percent",
-          "adj_asiannh_per" = "asiannh_percent",
-          "adj_amindnh_per" = "amindnh_percent",
-          "adj_othermultinh_per" = "othermultinh_percent",
-          "adj_hisppop_per" = "hisppop_percent",
-          "adj_nothisppop_per" = "nothisppop_percent",
-          "adj_meanhhi" = "meanhhinc",
-          "adj_novehicle_per" = "novehicle_percent",
-          "adj_lep_per" = "poorenglish_percent",
-          "adj_span_per" = "spanish_percent"
-        ) %>%
+      regionalparks.acs::block_group_map %>%
         select(selected_map_vars$input_acsmap)
     }
     return(p6)
--- R/mod_summary_utils.R
+++ R/mod_summary_utils.R
@@ -114,64 +114,17 @@ mod_summary_utils_server <- function(input, output, session,
         type %in% selected_vars$input_type,
         status %in% selected_vars$input_status,
         distance == selected_vars$input_distance
-      ) %>%
-      separate(name, into = c("name", "delete"), sep = "_")
+      )
     return(p5)
   })
   
-  tractdata <- tibble(ACS = c("adj_anydis_per", "adj_forborn_per", "adj_usborn_per"))
   
   make_map_bg_data <- reactive({
-    p6 <- if (selected_vars$input_acs %in% tractdata$ACS) (
-  regionalparks.acs::census_tract %>%
-        mutate(
-          disab_percent = `Disability, any disability` * 100,
-          usborn_percent = `Origin, US-born` * 100,
-          forborn_percent = `Origin, foreign-born` * 100
-        ) %>%
-        rename(
-          "adj_anydis_per" = "disab_percent",
-          "adj_usborn_per" = "usborn_percent",
-          "adj_forborn_per" = "forborn_percent"
-        ) %>%
+    p6 <- if (selected_vars$input_acs %in% tract_vars$ACS) {
+      regionalparks.acs::census_tract_map %>%
         select(selected_vars$input_acs)
-  )
-    
-    else {
-  regionalparks.acs::block_group %>%
-        mutate(
-          ageunder15_percent = ageunder15_percent * 100,
-          age15_24_percent = age15_24_percent * 100,
-          age25_64_percent = age25_64_percent * 100,
-          age65up_percent = age65up_percent * 100,
-          whitenh_percent = whitenh_percent * 100,
-          blacknh_percent = blacknh_percent * 100,
-          asiannh_percent = asiannh_percent * 100,
-          amindnh_percent = amindnh_percent * 100,
-          othermultinh_percent = othermultinh_percent * 100,
-          hisppop_percent = hisppop_percent * 100,
-          nothisppop_percent = nothisppop_percent * 100,
-          novehicle_percent = novehicle_percent * 100,
-          poorenglish_percent = poorenglish_percent * 100,
-          spanish_percent = spanish_percent * 100
-        ) %>%
-        rename(
-          "adj_ageunder15_per" = "ageunder15_percent",
-          "adj_age15_24_per" = "age15_24_percent",
-          "adj_age25_64_per" = "age25_64_percent",
-          "adj_age65up_per" = "age65up_percent",
-          "adj_whitenh_per" = "whitenh_percent",
-          "adj_blacknh_per" = "blacknh_percent",
-          "adj_asiannh_per" = "asiannh_percent",
-          "adj_amindnh_per" = "amindnh_percent",
-          "adj_othermultinh_per" = "othermultinh_percent",
-          "adj_hisppop_per" = "hisppop_percent",
-          "adj_nothisppop_per" = "nothisppop_percent",
-          "adj_meanhhi" = "meanhhinc",
-          "adj_novehicle_per" = "novehicle_percent",
-          "adj_lep_per" = "poorenglish_percent",
-          "adj_span_per" = "spanish_percent"
-        ) %>%
+    } else {
+      regionalparks.acs::block_group_map %>%
         select(selected_vars$input_acs)
     }
     return(p6)
