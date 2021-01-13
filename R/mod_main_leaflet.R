7879883bd215c0f0ae5f22b8ff2720b0a03647e2
Liz Roten <liz.roten@metc.state.mn.us>
2021-01-13 10:31:24 -0600

    implement base map module

--- R/mod_main_leaflet.R
+++ R/mod_main_leaflet.R
@@ -10,12 +10,12 @@
 mod_main_leaflet_ui <- function(id){
   ns <- NS(id)
   tagList(
- 
+    
     leafletOutput(ns("map"), width = "100%", height = 700)
     
   )
 }
-    
+
 #' main_leaflet Server Function
 #'
 #' @noRd 
@@ -23,124 +23,18 @@ mod_main_leaflet_server <- function(input, output, session,
                                     main_lft_inputs,
                                     current_tab){
   ns <- session$ns
- 
- output$map <- renderLeaflet({
-    leaflet() %>%
-    setView(
-      lat = 44.963,
-      lng = -93.22,
-      zoom = 9
-    ) %>%
-    addMapPane(name = "Carto Positron", zIndex = 430) %>%
-    addProviderTiles("CartoDB.PositronOnlyLabels",
-                     options = leafletOptions(pane = "Carto Positron"),
-                     group = "Carto Positron"
-    ) %>%
-    addProviderTiles("CartoDB.PositronNoLabels",
-                     group = "Carto Positron"
-    ) %>%
-    addProviderTiles(
-      provider = providers$Esri.WorldImagery,
-      group = "Aerial photography"
-    ) %>%
-    addMapPane("Agency boundaries", zIndex = 650) %>%
-    addPolygons(
-      data = agency_boundary,
-      group = "Agency boundaries",
-      stroke = T,
-      color = "black",
-      fill = F,
-      weight = 2,
-      options = pathOptions(pane = "Agency boundaries")
-    ) %>%
-     
-     addMapPane("water_access", zIndex = 431) %>%
-     addAwesomeMarkers(
-       group = "Water Access",
-       data = regionalparks.acs::water_access,
-       icon = iconwater,
-       options = pathOptions(pane = "water_access")
-     ) %>%
-     groupOptions(
-       group = "Water Access",
-       zoomLevels = 13:20
-     ) %>%
-     addMapPane("entrance", zIndex = 432) %>%
-     addAwesomeMarkers(
-       group = "Park Entrance",
-       data = regionalparks.acs::entrance,
-       icon = iconentry,
-       options = pathOptions(pane = "entrance")
-     ) %>%
-     groupOptions(
-       group = "Park Entrance",
-       zoomLevels = 13:20
-     ) %>%
-     addMapPane("trans", zIndex = 430) %>%
-     addCircles( # Markers(
-       data = regionalparks.acs::trans_stops,
-       group = "Active transit stops",
-       radius = 20,
-       fill = T,
-       stroke = TRUE,
-       weight = 2,
-       color = councilR::colors$transitRed,
-       fillColor = councilR::colors$transitRed,
-       options = pathOptions(pane = "trans")
-     ) %>%
-     groupOptions(
-       group = "Active transit stops",
-       zoomLevels = 13:20
-     ) %>%
-     addMapPane("riverlake", zIndex = 429) %>%
-     addPolygons(
-       data = regionalparks.acs::river_lake,
-       group = "Rivers & Lakes",
-       stroke = TRUE,
-       # weight = 0.5,
-       color = "black",
-       fill = TRUE,
-       fillColor = "black",
-       fillOpacity = 0.9,
-       options = pathOptions(pane = "riverlake")
-     ) %>%
-     hideGroup(
-       c("Buffers",
-         "Agency boundaries",
-         "Active transit stops",
-         "Water Access",
-         "Park Entrance",
-         "Rivers & Lakes"
-       )
-     ) %>%
-     addLayersControl(
-       position = "bottomright",
-       overlayGroups = c(
-         "Parks and trails",
-         "Buffers",
-         "Population data",
-         "Active transit stops",
-         "Water Access",
-         "Park Entrance",
-         "Rivers & Lakes",
-         "Agency boundaries"
-       ),
-       baseGroups = c(
-         "Carto Positron",
-         "Aerial photography"
-       ),
-       options = layersControlOptions(collapsed = T)
-     ) %>%
-     leaflet::addScaleBar(position = c("bottomleft"))
- })
- 
- # adding proxys --------
- 
+  
+  output$map <-  mod_map_base_server(input = input,
+                                     output = output,
+                                     session = session)
+  
+  # adding proxys --------
+  
  # add pop polygons -----
  toListen_mainleaflet <- reactive({
    list(current_tab,
      main_lft_inputs$map_bg_data_main,
-     main_lft_inputs$source,
+         main_lft_inputs$source,
      main_lft_inputs$mainacs,
      main_lft_inputs$mainpop
    )
