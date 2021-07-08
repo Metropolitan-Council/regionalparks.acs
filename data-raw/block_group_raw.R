bda03e6c16f84a03e95020c173cbf3afe8f7bc78
Ellen Esch <Ellen.Esch@metc.state.mn.us>
2021-07-08 17:26:49 -0500

    clean and update block group code

--- data-raw/block_group_raw.R
+++ data-raw/block_group_raw.R
@@ -29,86 +29,62 @@ fs::file_delete("CensusACSBlockGroup.xlsx")
 
 
 ## ----------------------------------------------------------------------------------------------------------------------------------------------------
-bg_age <- bg %>%
+race_key <- tibble(acscode = c("whitenh",
+              "blachnh",
+              "asiannh",
+              "amindnh",
+              "pacificnh",
+              "othernh",
+              "multracenh")) %>%
+  mutate(filtercode = paste0("race_", acscode))
+
+
+bg_merge <- bg %>%
+  select(geoid2, 
+         # household vars
+         hhtotal, 
+         hh_noveh, 
+         #economic vars
+         meanhhinc,
+         (starts_with("pov") & !ends_with("rate")),
+         #pop vars
+         poptotal,
+         lep, lep_span,
+         starts_with(c("m_", "f_")),
+         ends_with(c("nh", "hisppop"))) %>%
   mutate(
-    ageunder15 = ((m_0_4 + f_0_4 +
+    #### economic
+    pov185 = povertyn + poverty150 + pov150_185,
+    
+    ##### age
+    ### young -------
+    ageunder15 = m_0_4 + f_0_4 +
                      m_5_9 + f_5_9 +
-                     m_10_14 + f_10_14)),
+                     m_10_14 + f_10_14,
     # "youth"
     age15_24 = ((m_15_19 + f_15_19 +
                    m_20_24 + f_20_24)),
-    # "young adult"
-    age25_64 = ((m_25_29 + m_30_34 + m_35_39 + m_40_44 + m_45_49 + m_50_54 + m_55_59 + m_60_64 +
-                   f_25_29 + f_30_34 + f_35_39 + f_40_44 + f_45_49 + f_50_54 + f_55_59 + f_60_64))
-  ) %>%
-  transmute(
-    geoid2 = geoid2,
-    ageunder15_percent = round(ageunder15 / poptotal, digits = 2),
-    age15_24_percent = round(age15_24 / poptotal, digits = 2),
-    age25_64_percent = round(age25_64 / poptotal, digits = 2),
-    age25_64_percent = round(age25_64 / poptotal, digits = 2),
-    ageunder18_percent = round(ageunder18 / poptotal, digits = 2),
-    age18_39_percent = round(age18_39 / poptotal, digits = 2),
-    age40_64_percent = round(age40_64 / poptotal, digits = 2),
-    age65up_percent = round(age65up / poptotal, digits = 2)
-  )
-
-
-## -----------------------------------------------------------------------------------------------------------------------------------------------------
-bg_race <- bg %>%
-  transmute(
-    geoid2 = geoid2,
-    whitenh_percent = round(whitenh / poptotal, digits = 2),
-    blacknh_percent = round(blacknh / poptotal, digits = 2),
-    asiannh_percent = round(asiannh / poptotal, digits = 2),
-    amindnh_percent = round(amindnh / poptotal, digits = 2),
-    pacificnh_percent = round(pacificnh / poptotal, digits = 2),
-    othernh_percent = round(othernh / poptotal, digits = 2),
-    multracenh_percent = round(multracenh / poptotal, digits = 2),
-    poc_percent = 1 - whitenh_percent,
-    othermultinh_percent = round((pacificnh + othernh + multracenh) / poptotal, digits = 2),
-  )
-
-
-## -----------------------------------------------------------------------------------------------------------------------------------------------------
-bg_hisp <- bg %>%
-  transmute(
-    geoid2 = geoid2,
-    hisppop_percent = round(hisppop / poptotal, digits = 2),
-    nothisppop_percent = round(nothisppop / poptotal, digits = 2)
-  )
-
-
-# -----------------------------------------------------------------------------------------------------------------------------------------------------
-bg_transportation <- bg %>%
-  transmute(
-    geoid2 = geoid2,
-    novehicle_percent = round(hh_noveh / hhtotal, digits = 2)
-  )
-
-
-# -----------------------------------------------------------------------------------------------------------------------------------------------------
-
-bg_lang <- bg %>%
-  transmute(
-    geoid2 = geoid2,
-    poorenglish_percent = round(lep / poptotal, digits = 2),
-    spanish_percent = round(lep_span / poptotal, digits = 2)
-  )
-
-
-## -----------------------------------------------------------------------------------------------------------------------------------------------------
-bg_income <- bg %>%
-  select(geoid2, poptotal, medianhhi, meanhhinc, povertyn, poverty150, pov150_185, povdenom) %>%
-  mutate(pov185_percent = round((povertyn + poverty150 + pov150_185) / povdenom, digits = 2))
-
-
-## -----------------------------------------------------------------------------------------------------------------------------------------------------
-bg_merge <- right_join(bg_age, bg_race) %>%
-  right_join(bg_hisp) %>%
-  right_join(bg_income) %>%
-  right_join(bg_transportation) %>%
-  right_join(bg_lang)
+    # # "young adult"
+    # age25_64 = ((m_25_29 + m_30_34 + m_35_39 + m_40_44 + m_45_49 + m_50_54 + m_55_59 + m_60_64 +
+    #                f_25_29 + f_30_34 + f_35_39 + f_40_44 + f_45_49 + f_50_54 + f_55_59 + f_60_64)),
+    # "younger middle adult"
+    age25_44 = ((m_25_29 + m_30_34 + m_35_39 + m_40_44 + 
+                   f_25_29 + f_30_34 + f_35_39 + f_40_44)),
+    # "older middle adult"
+    age45_64 = ((m_45_49 + m_50_54 + m_55_59 + m_60_64 +
+                   f_45_49 + f_50_54 + f_55_59 + f_60_64)),
+    
+    ### race
+    othermultinh = pacificnh + othernh + multracenh) %>%
+  
+  select(-pacificnh, -othernh, -multracenh, 
+         -starts_with(c("m_", "f_")),
+         -povertyn, -poverty150, -pov150_185) %>% #aggregated, so don't need
+  mutate(
+    across(hh_noveh, ~round(.x / hhtotal, digits = 2)),
+    across(pov185, ~round(.x / povdenom, digits = 2)),
+    across(starts_with(c("age", "lep")) | ends_with(c("nh", "hisppop")), ~round(.x / poptotal, digits = 2))) 
+  
 
 
 ## -----------------------------------------------------------------------------------------------------------------------------------------------------
