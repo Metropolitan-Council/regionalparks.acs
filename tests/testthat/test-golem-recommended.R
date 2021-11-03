21c22be7f903083101b78aeac520eae50d45bc5a
Liz Roten <liz.roten@metc.state.mn.us>
2021-11-03 10:41:15 -0500

    use most up to date golem tests
    from golem 0.3.1

--- tests/testthat/test-golem-recommended.R
+++ tests/testthat/test-golem-recommended.R
@@ -1,50 +1,41 @@
-context("golem tests")
-
-library(golem)
-
 test_that("app ui", {
   ui <- app_ui()
-  expect_shinytaglist(ui)
+  golem::expect_shinytaglist(ui)
+  # Check that formals have not been removed
+  fmls <- formals(app_ui)
+  for (i in c("request")){
+    expect_true(i %in% names(fmls))
+  }
 })
 
 test_that("app server", {
   server <- app_server
-  expect_is(server, "function")
+  expect_type(server, "closure")
+  # Check that formals have not been removed
+  fmls <- formals(app_server)
+  for (i in c("input", "output", "session")){
+    expect_true(i %in% names(fmls))
+  }
+})
+
+# Configure this test to fit your need. 
+# testServer() function makes it possible to test code in server functions and modules, without needing to run the full Shiny application
+testServer(app_server, {
+  
+  # Set and test an input
+  # session$setInputs(x = 2)
+  # expect_equal(input$x, 2)
+  
+  # Example of tests you can do on the server: 
+  # - Checking reactiveValues 
+  # expect_equal(r$lg, 'EN')
+  # - Checking output
+  # expect_equal(output$txt, "Text")
 })
 
 # Configure this test to fit your need
 test_that(
-  "app launches",
-  # {
-  #   skip_on_cran()
-  #   skip_on_travis()
-  #   skip_on_appveyor()
-  #   skip_on_ci()
-  #   x <- processx::process$new(
-  #     "R",
-  #     c(
-  #       "-e",
-  #       "pkgload::load_all(here::here());run_app()"
-  #     )
-  #   )
-  #   Sys.sleep(5)
-  #   expect_true(x$is_alive())
-  #   x$kill()
-  # }
-  {
-    skip_on_cran()
-    skip_on_travis()
-    skip_on_appveyor()
-    skip_if_not(interactive())
-    x <- processx::process$new(
-      "R",
-      c(
-        "-e",
-        "pkgload::load_all(here::here());run_app()"
-      )
-    )
-    Sys.sleep(5)
-    expect_true(x$is_alive())
-    x$kill()
+  "app launches",{
+    golem::expect_running(sleep = 5)
   }
 )
